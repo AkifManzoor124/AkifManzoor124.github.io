b6a92d23511cf6a662a9f2ac9cdd4a53a7d4bf3b
Akif Manzoor <akifmanzoor124@gmail.com>
2020-09-17 16:37:34 -0400

    Updates

---
+++ precache-manifest.20f9ed39ee30fee8db0ff6f9f0969677.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "083d74d7e87520882225b5b19bbc35fc",
+    "url": "/AkifManzoor124.github.io/index.html"
+  },
+  {
+    "revision": "7406c1bae9e7a50b3770",
+    "url": "/AkifManzoor124.github.io/static/css/main.e8947b81.chunk.css"
+  },
+  {
+    "revision": "c0425df7bc258e204ae9",
+    "url": "/AkifManzoor124.github.io/static/js/2.5f57e9e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AkifManzoor124.github.io/static/js/2.5f57e9e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7406c1bae9e7a50b3770",
+    "url": "/AkifManzoor124.github.io/static/js/main.3f0fbf25.chunk.js"
+  },
+  {
+    "revision": "9e7dfee1862f164d269a",
+    "url": "/AkifManzoor124.github.io/static/js/runtime-main.cb5ed7ed.js"
+  },
+  {
+    "revision": "a72422c3600b6cdd624c9e6c0edb27af",
+    "url": "/AkifManzoor124.github.io/static/media/Akif.a72422c3.jpg"
+  },
+  {
+    "revision": "e9e71700c1d073e7b406bc40d971866c",
+    "url": "/AkifManzoor124.github.io/static/media/resume.e9e71700.jpg"
+  }
+]);
